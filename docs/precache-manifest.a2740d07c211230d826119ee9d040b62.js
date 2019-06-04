decf31d2ee17bf5fda557df56d96336ac8243b6e
De Freitas <drakoz1987@gmail.com>
2019-06-04 07:05:11 -0400

    made the npm build command and the docs folder

---
+++ docs/precache-manifest.a2740d07c211230d826119ee9d040b62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf5e3a329014bb9103d20452478096d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22ed5a350d184fc8e9af",
+    "url": "/static/js/2.566cede1.chunk.js"
+  },
+  {
+    "revision": "3744215d1a4e19a5cb51",
+    "url": "/static/js/main.ee51d75c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
